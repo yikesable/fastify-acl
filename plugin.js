d23e41456740a26158d1448032ac8d04732de55f
Pelle Wessman <pelle@kodfabrik.se>
2021-01-12 16:26:39 +0100

    Add typescript, remove fastify-url-data

--- plugin.js
+++ plugin.js
@@ -1,74 +1,104 @@
 'use strict'
 
-const fp = require('fastify-plugin')
+const fp = require('fastify-plugin').default
 const debug = require('debug')('fastify-acl-auth:plugin')
 const UrlPattern = require('url-pattern')
 
-const auth = require('./lib/auth')
-const util = require('./lib/util')
+const { checkRoles } = require('./lib/auth')
+const { getRoles } = require('./lib/util')
 
+/**
+ * @typedef HookFactoryOptions
+ * @property {import('./lib/util').RoleArgument} [actualRoles]
+ * @property {import('./lib/util').RoleArgument} [allowedRoles]
+ * @property {string[]} [pathExempt]
+ */
+
+/** @typedef {HookFactoryOptions & import('./lib/auth').CheckRolesOptions} FastifyAclAuthOptions */
+
+/** @type {FastifyAclAuthOptions}  */
 const defaults = {
-  actualRoles: function (request) {
-    let _return
-    try {
-      _return = request.session.credentials.roles
-    } catch {
-      _return = []
-    }
-    return _return
-  },
-  any: true,
+  actualRoles: request =>
+    // @ts-ignore
+    (((request || {}).session || {}).credentials || {}).roles || [],
   all: false
 }
 
-const hookFactory = function (_fastify, options) {
+/**
+ * @param {FastifyAclAuthOptions} options
+ * @returns {import('fastify').preHandlerHookHandler}
+ */
+const hookFactory = (options) => {
+  const {
+    actualRoles,
+    allowedRoles
+  } = options;
+
   const urlPatterns = (options.pathExempt || []).map(pathPattern => new UrlPattern(pathPattern))
 
+  /**
+   * @param {string} path
+   * @returns {boolean}
+   */
   const pathExempt = (path) => urlPatterns.some(urlPattern => urlPattern.match(path));
 
+  /** @type {import('fastify').preHandlerHookHandler} */
   return async function (request, reply) {
-    debug(`hook called for ${request.raw.originalUrl}`)
-    try {
-      const _actual = await util.getRoles(options.actualRoles, request)
-      const _allowed = await util.getRoles(options.allowedRoles, request)
-      let isAuthorized = await auth.isAuthorized(_actual, _allowed, options)
-      if (options.pathExempt) {
-        debug('options.pathExempt is set')
-        if (pathExempt(request.raw.originalUrl)) {
-          debug('options.pathExempt does match URL, overriding isAuthorized (setting to true)')
-          isAuthorized = true
-        }
-      }
-      debug('_actual: %j', _actual)
-      debug('_allowed: %j', _allowed)
-      debug('isAuthorized: %j', isAuthorized)
-      if (!isAuthorized) {
-        return reply.code(403).send()
-      }
-    } catch (err) {
-      debug('ERROR: in hook: %s', err.message)
-      return err
+    debug(`hook called for ${request.url}`)
+
+    const [actual, allowed] = await Promise.all([
+      getRoles(actualRoles, request),
+      getRoles(allowedRoles, request)
+    ]);
+
+    let isAuthorized;
+
+    if (pathExempt && pathExempt(request.url)) {
+      debug('options.pathExempt does match URL, overriding isAuthorized (setting to true)')
+      isAuthorized = true
     }
+
+    if (isAuthorized === undefined) {
+      isAuthorized = await checkRoles(actual, allowed, options)
+    }
+
+    debug('actual: %j', actual)
+    debug('allowed: %j', allowed)
+    debug('isAuthorized: %j', isAuthorized)
+
+    // TODO: Make code configurable. Maybe throw an error object instead?
+    return isAuthorized ? undefined : reply.code(403).send()
   }
 }
 
-const pluginFactory = function (options) {
+/**
+ * @param {FastifyAclAuthOptions} [options]
+ * @returns {import('fastify').FastifyPluginAsync}
+ */
+const pluginFactory = function (options = {}) {
   debug('pluginFactory() called')
-  const instanceOptions = Object.assign({}, defaults, options)
+  const instanceOptions = {
+    ...defaults,
+    ...options
+  };
+
   debug('instanceOptions: %j', instanceOptions)
-  return fp(
-    async function (fastify, options) {
-      debug('plugin() called')
-      fastify.register(require('fastify-url-data'))
-      const pluginOptions = Object.assign({}, instanceOptions, options)
-      debug('pluginOptions: %j', pluginOptions)
-      const hook = hookFactory(fastify, pluginOptions)
-      fastify.addHook('preHandler', hook)
-    },
-    {
-      fastify: '>=3.0.0'
-    }
-  )
+
+  /** @type {import('fastify').FastifyPluginAsync} */
+  const plugin = async function (fastify, options) {
+    debug('plugin() called')
+
+    const pluginOptions = {
+      ...instanceOptions,
+      ...options
+    };
+
+    debug('pluginOptions: %j', pluginOptions)
+
+    fastify.addHook('preHandler', hookFactory(pluginOptions))
+  };
+
+  return fp(plugin, { fastify: '>=3.0.0' })
 }
 
 module.exports = pluginFactory
